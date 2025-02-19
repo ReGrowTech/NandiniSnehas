b0ed836deb8127ca5a84d26afe59ee5420cbada8
ftariq <faizant@mindfiresolutions.com>
2025-02-19 12:05:11 +0530

    I have added addition message

--- src/test/java/testCases/SC001.java
+++ src/test/java/testCases/SC001.java
@@ -5,7 +5,7 @@ public class SC001 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello my friends!!!");
+		System.out.println("Hello my friends. This my first git class!!!");
 
 	}
 
