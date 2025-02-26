c009c6ee5bee2a71be81586fcd95229ac1cf278e
Snehas-Jena <snehasjena10@gmail.com>
2025-02-26 11:35:07 +0530

    Snehas has added code to SC001

--- src/test/java/testCases/SC001.java
+++ src/test/java/testCases/SC001.java
@@ -4,7 +4,7 @@ public class SC001 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        System.out.println("We are from Regrowtech");
 		System.out.println("Hello my friends. This my first git class!!!");
 
 	}
